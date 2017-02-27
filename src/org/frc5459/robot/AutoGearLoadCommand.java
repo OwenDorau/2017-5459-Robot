f1b3e470315d6cabcbc7c4ec4030ee94deb25e37
Owen Dorau <ODorau@gmail.com>
2017-02-27 16:50:49 -0500

    Changed logic in AutoGearCommand

--- src/org/frc5459/robot/AutoGearLoadCommand.java
+++ src/org/frc5459/robot/AutoGearLoadCommand.java
@@ -27,25 +27,30 @@ public class AutoGearLoadCommand extends Command {
 	double Second ;
 	@Override
 	public boolean execute(){
+		first = true;
+		if(first = true){
 		DoubleSupplier FirstTargetAngle= () -> {return (0) ;} ;
 		DoubleConsumer FirstTurnThis  = (x) -> First = x  ;
-		
+		System.out.println("First Supplier and Consumer passed");
 		targetTurn = new TurnToCommand (FirstTargetAngle , FirstTurnThis);
 		drive.setSpeedRight(-1.0);
 		drive.setSpeedLeft(-1.0);
-		first = true;
-		if(first = true){
 			ultraSonicX = drive.getUltrasonicX();
 			deltaUltraXDistance = ultraSonicX - ultraXDistance;
 			if(deltaUltraXDistance > (1.2)){
 				drive.setSpeedRight(0.0);
 				drive.setSpeedLeft(0.0);
+				first = false;
 				second = true;
-			}
+				}else{
+					drive.setSpeedRight(-1.0);
+					drive.setSpeedLeft(-1.0);
+				}
+			System.out.println("First Done");
 		}
 		DoubleSupplier SecondTargetAngle= () -> {return (45) ;} ;
 		DoubleConsumer SecondTurnThis  = (x) -> Second = x  ;
-		
+		System.out.println("Second Supplier and Consumer passed");
 		targetTurn = new TurnToCommand (SecondTargetAngle , SecondTurnThis);
 		if(second = true){
 			Strongback.submit(targetTurn);
@@ -57,6 +62,7 @@ public class AutoGearLoadCommand extends Command {
 				drive.setSpeedRight(0.0);
 				drive.setSpeedLeft(0.0);
 			}
+			System.out.println("Second Done");
 		}	
 		return false;
 	}
